1b2926d828a836070774438830960a98899f426d
Sofia Scherer <sofipscherer@gmail.com>
2025-10-13 23:48:24 -0300

    Refazendo tudo kk

--- main.c
+++ main.c
@@ -1,17 +1,7 @@
-#include "trab1.h"
+#include "utils.h"
 
 
 int main(){
 
-    peca** board = aloca_matriz(6, 3);
-    montar_prob1(board);
-    print_board(board, 6, 3);
-    free(board);
-    printf("\n\n");
-
-    board = aloca_matriz(4, 4);
-    montar_prob2(board);
-    print_board(board, 4, 4);
-
-    a_estrela();
+    
 }
--- utils.c
+++ utils.c
@@ -1,87 +1,170 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "utils.h"
 
 /*
-PRETO 1
-BRANCO 2
+    COR:
+        0 VAZIO
+        1 BRANCO
+        2 PRETO
 
-PEAO 1
-TORRE 2
-UMA MUSUME/CAVALO 3
-BISPO 4
-RAINHA 5
-REI 6
+    PEÇA:
+        0 VAZIO
+        1 PEAO
+        2 TORRE
+        3 CAVALO
+        4 BISPO
+        5 RAINHA
+        6 REI
 */
+typedef struct tile tile_t;
+typedef struct no no_t;
+typedef struct no_fila no_fila_t;
+typedef struct fila fila_t;
 
-peca** aloca_matriz(int lar, int alt){
+// Definição da struct tile
+struct tile {
+    int cor;
+    int tipo;
+};
 
-    peca** matriz = calloc( alt, sizeof(peca*));
-    for (int i = 0; i<alt; i++){
-        matriz[i] = calloc( lar, sizeof(peca));
+// Definição da struct no
+struct no {
+    tile_t** board;
+    no_t* pai;
+};
+
+// Definição da struct no_fila
+struct no_fila {
+
+};
+
+// Definição da struct fila
+struct fila {
+    no_fila_t* primeiro;
+    no_fila_t* ultimo;   // Adicionei um ponteiro para o último para eficiência
+    int tam;
+};
+
+tile_t** init_prob1(){
+
+    tile_t** board = calloc(3, sizeof(tile_t*));
+    for(int i = 0; i<4; i++){
+        board[i] = calloc(6, sizeof(tile_t));
     }
-    return matriz;
-}
 
-void ini_mesa(peca** board){
-
-    //PRETAS
-    for (int i=0; i<8; i++){
-        board[0][i].cor  = 1;
-    }
-    //TORRES
-    board[0][0].tipo = 2;
-    board[0][7].tipo = 2;
-    //CAVALOS
-    board[0][1].tipo = 3;
-    board[0][6].tipo = 3;
-    //BISPOS
+    board[0][0].cor = 2;
+    board[0][0].tipo = 3;
+    board[0][1].cor = 1;
+    board[0][1].tipo = 4;
+    board[0][2].cor = 1;
     board[0][2].tipo = 4;
-    board[0][5].tipo = 4;
-    //RAINHA
-    board[0][3].tipo = 5;
-    //REI
-    board[0][4].tipo = 6;
-    //PEOES
-    for (int i=0; i<8; i++){
-        board[1][i].cor  = 1;
-        board[1][i].tipo = 1;
-    }
+    board[0][3].cor = 1;
+    board[0][3].tipo = 4;
+    board[0][4].cor = 1;
+    board[0][4].tipo = 4;
+    board[0][5].cor = 1;
+    board[0][5].tipo = 2;
 
-    //BRANCAS
-    for (int i=0; i<8; i++){
-        board[7][i].cor  = 2;
-    }
-    //TORRES
-    board[7][0].tipo = 2;
-    board[7][7].tipo = 2;
-    //CAVALOS
-    board[7][1].tipo = 3;
-    board[7][6].tipo = 3;
-    //BISPOS
-    board[7][2].tipo = 4;
-    board[7][5].tipo = 4;
-    //RAINHA
-    board[7][3].tipo = 5;
-    //REI
-    board[7][4].tipo = 6;
-    //PEOES
-    for (int i=0; i<8; i++){
-        board[6][i].cor  = 2;
-        board[6][i].tipo = 1;
-    }
+    board[1][0].cor = 1;
+    board[1][0].tipo = 3;
+    board[1][1].cor = 1;
+    board[1][1].tipo = 3;
+    board[1][2].cor = 1;
+    board[1][2].tipo = 3;
+    board[1][3].cor = 1;
+    board[1][3].tipo = 3;
+    board[1][4].cor = 1;
+    board[1][4].tipo = 2;
+    board[1][5].cor = 1;
+    board[1][5].tipo = 2;
+
+    board[2][0].cor = 0;
+    board[2][0].tipo = 0;
+    board[2][1].cor = 0;
+    board[2][1].tipo = 0;
+    board[2][2].cor = 0;
+    board[2][2].tipo = 0;
+    board[2][3].cor = 0;
+    board[2][3].tipo = 0;
+    board[2][4].cor = 1;
+    board[2][4].tipo = 2;
+    board[2][5].cor = 0;
+    board[2][5].tipo = 0;
+
+    return board;
 }
 
-void print_board(peca** board, int lar, int alt){
-
-    for (int i=0; i<alt; i++){
-        for (int j=0; j<lar;j++){
-            printf("%d ", board[i][j].tipo);
-        }
-        printf("    ");
-        for (int j=0; j<lar;j++){
-            printf("%d ", board[i][j].cor);
-        }
-        printf("\n");
+tile_t** init_prob2(){
+    
+    tile_t** board = calloc(4, sizeof(tile_t*));
+    for(int i = 0; i<5; i++){
+        board[i] = calloc(4, sizeof(tile_t));
     }
+
+    board[0][0].cor = 1;
+    board[0][0].tipo = 3;
+    board[0][1].cor = 1;
+    board[0][1].tipo = 3;
+    board[0][2].cor = 1;
+    board[0][2].tipo = 3;
+    board[0][3].cor = 1;
+    board[0][3].tipo = 3;
+    board[0][4].cor = 1;
+    board[0][4].tipo = 3;
+
+    board[1][0].cor = 1;
+    board[1][0].tipo = 4;
+    board[1][1].cor = 1;
+    board[1][1].tipo = 4;
+    board[1][2].cor = 1;
+    board[1][2].tipo = 4;
+    board[1][3].cor = 1;
+    board[1][3].tipo = 4;
+    board[1][4].cor = 1;
+    board[1][4].tipo = 4;
+
+    board[2][0].cor = 1;
+    board[2][0].tipo = 2;
+    board[2][1].cor = 1;
+    board[2][1].tipo = 2;
+    board[2][2].cor = 1;
+    board[2][2].tipo = 2;
+    board[2][3].cor = 1;
+    board[2][3].tipo = 2;
+    board[2][4].cor = 1;
+    board[2][4].tipo = 2;
+
+    board[3][0].cor = 1;
+    board[3][0].tipo = 2;
+    board[3][1].cor = 1;
+    board[3][1].tipo = 2;
+    board[3][2].cor = 1;
+    board[3][2].tipo = 2;
+    board[3][3].cor = 1;
+    board[3][3].tipo = 2;
+    board[3][4].cor = 1;
+    board[3][4].tipo = 2;
+
+    return board;
+}
+
+fila_t* init_fila(){
+
+    fila_t* A = calloc(1, sizeof(fila_t));
+    A->primeiro = NULL;
+    A->tam = 0;
+
+    return A;
+}
+
+no_t* cria_no(tile_t** board, no_t* pai){
+
+    no_t* node = calloc(1, sizeof(no_t));
+    node->board = board;
+    node->pai = pai;
+
+    return node;
+}
+
+void insere_no(fila_t* ){
+    
 }
